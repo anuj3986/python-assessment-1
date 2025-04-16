806245e720d4d4b7e4c8f16ad0a4d8ac1f647f80
anuj3986 <96497406+anuj3986@users.noreply.github.com>
2025-04-16 20:16:46 +0530

    Add files via upload
    Added two files : 1) Simple calculation 2) concatenate first and last name

---
+++ assignment_1_add-sub-mul-div.py
@@ -0,0 +1,12 @@
+a = input('Enter the First value: ')
+a  = int(a)
+b = input('Enter the Second value: ')
+b = int(b)
+add = a+b
+sub = a-b
+mul = a*b
+div = a/b
+print('Addition: ', add)
+print('Substract: ', sub)
+print('Multiply: ', mul)
+print('Divide: ', div)
